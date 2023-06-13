b973709064c6cb070086e5e2ac3a72c1aaa11950
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-12 22:03:28 -0400

    Add remove notefunctionality

--- src/notes.js
+++ src/notes.js
@@ -20,3 +20,14 @@ export const findNotes = async (filter) => {
     const {notes} = await getDB();
     return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()));
 }
+
+export const removeNote = async (id) => {
+    const {notes} = await getDB();
+    const match = notes.find(note => note.id === id);
+    
+    if (match) {
+        const newNotes = notes.filter(note => note.id !== id);
+        await saveDB({notes: newNotes});
+        return id;
+    }
+}
