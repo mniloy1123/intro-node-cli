9631b5392989a1594626740679cf7aa015353f3c
Niloy <106124483+mniloy1123@users.noreply.github.com>
2023-06-12 21:52:10 -0400

    Add create, read, and update notes functionality

---
+++ src/notes.js
@@ -0,0 +1,22 @@
+import { insertDB, saveDB, getDB } from "./db";
+
+export const newNote = async (note, tags) => {
+    const newNote = {
+        tags,
+        id: Date.now(),
+        content: note,
+    }
+
+    await insertDB(newNote);
+    return newNote;
+}
+
+export const getAllNotes = async () => {
+    const {notes} = await getDB();
+    return notes;
+}
+
+export const findNotes = async (filter) => {
+    const {notes} = await getDB();
+    return notes.filter(note => note.content.toLowerCase().includes(filter.toLowerCase()));
+}
